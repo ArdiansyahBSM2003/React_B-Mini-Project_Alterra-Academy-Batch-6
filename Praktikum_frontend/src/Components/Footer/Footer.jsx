55686f80d98f6802ec69f67aedb6c555b474f340
11211016@student.itk.ac.id <11211016@student.itk.ac.id>
2024-05-14 07:59:09 +0800

    Submit MinPro

--- Praktikum_frontend/src/Components/Footer/Footer.jsx
+++ Praktikum_frontend/src/Components/Footer/Footer.jsx
@@ -6,9 +6,9 @@ const Footer = () => {
     <>
       {/* Footer section */}
       <footer className="font-roboto">
-        <div className="footer p-10 bg-gray-400 text-gray-content flex-wrap">
+        <div className="footer p-10 bg-gray-400 text-gray-content flex-wrap justify-center md:justify-between">
           {/* Services section */}
-          <nav className="mb-4 ml-32">
+          <nav className="mb-4">
             <h6 className="footer-title">Services</h6>
             <a className="link link-hover">Branding</a>
             <a className="link link-hover">Design</a>
@@ -16,7 +16,7 @@ const Footer = () => {
             <a className="link link-hover">Advertisement</a>
           </nav>
           {/* Company section */}
-          <nav className="mb-4 ml-32">
+          <nav className="mb-4">
             <h6 className="footer-title">Company</h6>
             <Link to="/aboutUs" className="link link-hover">
               About us
@@ -32,7 +32,7 @@ const Footer = () => {
             </Link>
           </nav>
           {/* Legal section */}
-          <nav className="mb-4 ml-32">
+          <nav className="mb-4">
             <h6 className="footer-title">Legal</h6>
             <a className="link link-hover">Terms of use</a>
             <a className="link link-hover">Privacy policy</a>
@@ -59,9 +59,9 @@ const Footer = () => {
           </form>
         </div>
         {/* Bottom section */}
-        <div className="footer items-center p-4 bg-neutral text-neutral-content">
+        <div className="footer items-center p-4 bg-neutral text-neutral-content flex flex-wrap justify-center md:justify-between">
           {/* Copyright section */}
-          <aside className="items-center grid-flow-col">
+          <aside className="items-center mb-4 md:mb-0">
             <svg
               width="36"
               height="36"
@@ -69,13 +69,13 @@ const Footer = () => {
               xmlns="http://www.w3.org/2000/svg"
               fillRule="evenodd"
               clipRule="evenodd"
-              className="fill-current">
+              className="fill-current mr-2">
               <path d="M22.672 15.226l-2.432.811.841 2.515c.33 1.019-.209 2.127-1.23 2.456-1.15.325-2.148-.321-2.463-1.226l-.84-2.518-5.013 1.677.84 2.517c.391 1.203-.434 2.542-1.831 2.542-.88 0-1.601-.564-1.86-1.314l-.842-2.516-2.431.809c-1.135.328-2.145-.317-2.463-1.229-.329-1.018.211-2.127 1.231-2.456l2.432-.809-1.621-4.823-2.432.808c-1.355.384-2.558-.59-2.558-1.839 0-.817.509-1.582 1.327-1.846l2.433-.809-.842-2.515c-.33-1.02.211-2.129 1.232-2.458 1.02-.329 2.13.209 2.461 1.229l.842 2.515 5.011-1.677-.839-2.517c-.403-1.238.484-2.553 1.843-2.553.819 0 1.585.509 1.85 1.326l.841 2.517 2.431-.81c1.02-.33 2.131.211 2.461 1.229.332 1.018-.21 2.126-1.23 2.456l-2.433.809 1.622 4.823 2.433-.809c1.242-.401 2.557.484 2.557 1.838 0 .819-.51 1.583-1.328 1.847m-8.992-6.428l-5.01 1.675 1.619 4.828 5.011-1.674-1.62-4.829z"></path>
             </svg>
             <p>Copyright © 2024 - Ardiansyah Bsm</p>
           </aside>
           {/* Social media icons section */}
-          <nav className="grid-flow-col gap-4 md:place-self-center md:justify-self-end w-full md:w-auto">
+          <nav className="grid grid-cols-3 gap-4">
             <a>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
